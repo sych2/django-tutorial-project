346fdd858c7a5908df5ef84f53980c6b23084960
Sych <mainasimon08@gmail.com>
2026-01-05 23:27:00 +0300

    db migrations

--- backend/api/models.py
+++ backend/api/models.py
@@ -15,6 +15,19 @@ class MyAccountManager(BaseUserManager):
         )
         user.set_password(password)
         user.save(using=self._db)
+        return user
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
 def get_profile_image_filepath(self, filename):
     return f'profile_images/{self.pk}/{"profile_image.png"}'
 
@@ -23,10 +36,10 @@ def get_default_profile_image():
 
 
 class account(AbstractBaseUser):
-    email=models.EmailField(verbose_name="email", max_length=60, unuique=True)
-    username=models.CharField(maxlength=30, unique=True)
-    date_joined=models.DateTimeField(verbose_name="data joined", auto_now_true=True)
-    last_joined=models.DateTimeField(verbose_name="last joined", auto_now_true=True)
+    email=models.EmailField(verbose_name="email", max_length=60, unique=True)
+    username=models.CharField(max_length=30, unique=True)
+    date_joined=models.DateTimeField(verbose_name="data joined", auto_now=True)
+    last_joined=models.DateTimeField(verbose_name="last joined", auto_now=True)
     is_admin=models.BooleanField(default=False)
     is_active=models.BooleanField(default=False)
     is_staff=models.BooleanField(default=False)
@@ -34,6 +47,7 @@ class account(AbstractBaseUser):
     profile_image=models.ImageField(max_length=255, upload_to=get_profile_image_filepath)
     hide_email=models.BooleanField(default=True)
    
+    objects = MyAccountManager()
    #Required to create an account 
     USERMNAME_FIELD="email"
     REQUIRED_FIELDS=["username"]
