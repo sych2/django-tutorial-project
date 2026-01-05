ddde839432b1d1311472faf91a4ba5a700d33a6e
Sych <mainasimon08@gmail.com>
2026-01-05 09:52:15 +0300

    latest change

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -15,5 +15,5 @@ class UserSerializer(serializers.ModelSerializer):
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = ["id", "title", "content", "created_at", "autho r"]
+        fields = ["id", "title", "content", "created_at", "author"]
         extra_kwargs = {"author": {"read_only": True}},
