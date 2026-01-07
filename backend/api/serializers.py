35ed218f819974d9dd92b5755c442c1008e75d74
Sych <mainasimon08@gmail.com>
2026-01-07 20:54:29 +0300

    models changes

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,13 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import  account
+from .models import Note
 
+User = get_user_model()
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
 
 
 
