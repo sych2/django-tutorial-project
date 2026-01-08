528270b64ca76a980c94ddbc179324942bcda9a6
Sych <mainasimon08@gmail.com>
2026-01-08 13:27:18 +0300

    minor changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = [
     'corsheaders'
 ]
 
-AUth_USER_MODEL = "acoount.Account"
+AUth_USER_MODEL = "api.Account"
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
