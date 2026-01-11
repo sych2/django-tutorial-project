8f28ecf8caf2492dba69be8a4dfc9a908ae5b4e2
Sych <mainasimon08@gmail.com>
2026-01-12 01:11:37 +0300

    store

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin, UserManager 
+import datetime
+
 
 
 class MyAccountManager(UserManager):
@@ -30,7 +32,6 @@ class MyAccountManager(UserManager):
 
 class account(AbstractUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-
     profile_image = models.ImageField(
         upload_to = '../profile_images',
         blank=True,
@@ -47,3 +48,42 @@ class account(AbstractUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name  
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+#All of our products
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default = 0, decimal_places=2, max_digits=6 )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1) 
+    description = models.CharField(max_length=250, default='', blank = True, null= True )
+    image = models.ImageField(upload_to='uploads/products/' )
+    
+    def __str__(self):
+        return self.email
+
+#Customer Orders
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete= models.CASCADE)
+    qualtity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='', blank=True)
+    phone = models.CharField(max_length=50)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path 
 from django.urls import path, include
+from . import settings
+from django.conf.urls.static import static
 
 
 
@@ -9,4 +11,4 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('home/', include('store.urls')),
    
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
