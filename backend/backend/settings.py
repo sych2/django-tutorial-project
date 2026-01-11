a3c390a7add6c7901906a6339daa999f702ba43c
Sych <mainasimon08@gmail.com>
2026-01-11 22:09:56 +0300

    cart

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'cart',
     'rest_framework',
     'corsheaders'
 ]
---
+++ backend/cart/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('', views.cart_summary, name="cart_summary"),
+    path('add/', views.cart_add, name="cart_add"),
+    path('delete/', views.cart_delete, name="cart_delete"),
+    path('update', views.cart_update, name="cart_update"), 
+    
+]
+
