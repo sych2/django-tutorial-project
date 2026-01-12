11d0dcb7af757bd9c8dd516090a865bf2e4bf76c
Sych <mainasimon08@gmail.com>
2026-01-13 00:39:23 +0300

    templates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = ['static/']
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
