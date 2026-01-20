7f4fa0f8eab2c9070a5b55b4c3920a9978b7c947
Sych <mainasimon08@gmail.com>
2026-01-21 00:13:36 +0300

    latest changes

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 urlpatterns = [ 
     path('', views.home, name = 'home'),
+    path('about/', views.about, name = 'about'),
    
 ]
--- backend/store/views.py
+++ backend/store/views.py
@@ -3,4 +3,7 @@ from .models import Product
 # Create your views here.
 def home(request):
     products = Product.objects.all()
-    return render(request, 'home.html', {'products': products}) 
+    return render(request, 'home.html', {'products': products})
+def about(request):
+    
+    return render(request, 'about.html') 
