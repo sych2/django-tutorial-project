c99fbeb841d9cecb3a063b4b1711ce4638979b54
Sych <mainasimon08@gmail.com>
2026-01-23 01:39:19 +0300

    latest changes

--- backend/store/views.py
+++ backend/store/views.py
@@ -17,7 +17,7 @@ def login_user(request):
         user = authenticate(request, username=username, password = password)
         if user is not None:
             login(request, user)
-            messages.success(request, ("You have logged in successfully"))
+            messages.success(request, ("You have been logged in successfully"))
             return redirect('home')
         else:
             messages.success(request, ("Your credentials don't match"))
