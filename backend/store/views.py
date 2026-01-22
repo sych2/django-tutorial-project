12823242740a9efd82b323a890d0f11614179c53
Sych <mainasimon08@gmail.com>
2026-01-23 00:53:58 +0300

    latest changes

--- backend/store/views.py
+++ backend/store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -11,9 +11,24 @@ def about(request):
     return render(request, 'about.html',{} ) 
 
 def login_user(request):
+    if request.method == "POST":#validate the post request
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password = password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have logged in successfully"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Your credentials don't match"))
+            return redirect('login')
     
-    return render(request, 'login.html',{} )
+    else:
+        return render(request, 'login.html', {})
+    
+    
 
 def logout_user(request):
-    
-    pass
+    logout(request)
+    messages.success(request, ("you have been logged out"))
+    return redirect('login')
