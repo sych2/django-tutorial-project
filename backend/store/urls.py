dfd24d6d8a59a935c705f60697ebe0b6b0205da9
Sych <mainasimon08@gmail.com>
2026-01-23 21:51:03 +0300

    latest changes

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('about/', views.about, name = 'about'),
     path('login/', views.login_user, name = 'login'),
     path('logout/', views.logout_user, name = 'logout'),
+    path('register/', views.register_user, name = 'register'),
+    
    
 ]
--- backend/store/views.py
+++ backend/store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Product 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 # Create your views here.
 def home(request):
     products = Product.objects.all()
@@ -32,3 +33,24 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("you have been logged out"))
     return redirect('login')
+
+def register_user(request):
+    form = SignUpForm
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            #login user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("you have registered successfully"))
+            return redirect('home')
+        
+    else:
+        messages.success(request, ("registration failed please try again"))
+        return redirect('register')
+
+    return render(request, 'register.html', {'form':form})
