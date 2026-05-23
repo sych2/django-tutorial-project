6f1bb384442932efd6ac89e0a9fa7fe3dd402824
Sych <mainasimon08@gmail.com>
2026-05-24 00:47:41 +0300

    oauth up and runing

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,5 +1,8 @@
+from authentication.views import (CompleteProfileView,
+                                  GoogleOAuthJWTExchangeView)
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 
 from . import settings
@@ -7,6 +10,7 @@ from . import settings
 
 def health(request):
     return JsonResponse({"status": "ok"})
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -17,5 +21,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('api/auth/google/token/', GoogleOAuthJWTExchangeView.as_view()),
     path('api/auth/complete-profile/', CompleteProfileView.as_view()),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
